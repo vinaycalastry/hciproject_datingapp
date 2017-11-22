40e9bd852bfebdfffc2436e322c9d6a294a96d05
Vinay <vinay.calastry@gmail.com>
2017-11-21 16:01:32 -0800

    Added google login

--- FrontEnd/TestAmante/app/src/main/java/com/example/android/testamante/ui/LoginActivity.java
+++ FrontEnd/TestAmante/app/src/main/java/com/example/android/testamante/ui/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -14,10 +15,18 @@ import android.widget.Toast;
 import com.example.android.testamante.MainActivity;
 import com.example.android.testamante.R;
 import com.example.android.testamante.utils.UtilClass;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -26,6 +35,8 @@ public class LoginActivity extends AppCompatActivity {
     private AutoCompleteTextView mUserPasswordView;
     private CheckBox rememberLoginBox;
     private FirebaseAuth auth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -38,6 +49,7 @@ public class LoginActivity extends AppCompatActivity {
         mUserPasswordView=(AutoCompleteTextView) findViewById(R.id.loginUserPassword);
         rememberLoginBox=(CheckBox)findViewById(R.id.loginRememberMeCheckBox) ;
 
+
         Button signInUserBtn = (Button) findViewById(R.id.loginSignInBtn);
         signInUserBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,9 +59,23 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         Button googleSignInUserBtn = (Button) findViewById(R.id.loginSignInGoogleBtn);
+        googleSignInUserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptGoogleLogin();
+            }
+        });
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
     }
 
     /**
@@ -126,12 +152,70 @@ public class LoginActivity extends AppCompatActivity {
                                 }
                             }
                         });
+
+
             } else
                 UtilClass.showToast(this, getResources().getString(R.string.network_error));
         }
     }
 
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
 
+        if (UtilClass.isNetworkAvailable(this)) {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+
+
+            AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+            auth.signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                FirebaseUser user = auth.getCurrentUser();
+                                Intent intent = new Intent(com.example.android.testamante.ui.LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(com.example.android.testamante.ui.LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }
+                    });
+
+
+            } else
+                UtilClass.showToast(this, getResources().getString(R.string.network_error));
+        }
+
+    private void attemptGoogleLogin() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                //Log.w(TAG, "Google sign in failed", e);
+
+            }
+        }
+    }
     public boolean isUserNameValid(String username) {
         //TODO: Replace this with your own logic
         return true;
