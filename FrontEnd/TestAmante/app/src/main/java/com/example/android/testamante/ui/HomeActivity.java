a5a5e92d5b57f760010717d00aa65b01079ced68
Aditya Rajuladevi <aditya.rajuladevi@unlv.edu>
2017-11-21 19:22:19 -0800

    Issues Fix after Code Removal

--- FrontEnd/TestAmante/app/src/main/java/com/example/android/testamante/ui/HomeActivity.java
+++ FrontEnd/TestAmante/app/src/main/java/com/example/android/testamante/ui/HomeActivity.java
@@ -1,8 +1,8 @@
 package com.example.android.testamante.ui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,7 +15,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_layout);
     }
 
     @Override
