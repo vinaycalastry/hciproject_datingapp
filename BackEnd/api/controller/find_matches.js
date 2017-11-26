cfd17dfd7b4431d7f20f06f95b9e5c67dc264090
stathari <kmit.stathari@gmail.com>
2017-11-25 22:27:45 -0800

    find matched profiles
    node js find matches code

--- BackEnd/api/controller/find_matches.js
+++ BackEnd/api/controller/find_matches.js
@@ -26,23 +26,52 @@ module.exports.getMatches = function(req, res){
         var dislikes = snapshot.child('dislikes').val();
 
         var matchedPeople = matchDate(profileId, likes, dislikes);
-        
-        updatedMatchesRDB(profileId, matchedPeople);
+		console.log(profileId);
 
       }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
       });
     
-    res.status(200).json("Ended");    
+    res.status(200).json("1");    
 }
 
 
 //Match with potential algorithm
 function matchDate(profId, likeList, dislikeList){
-    console.log(profId);
-    console.log('-----');
-    console.log(likeList);
-    var matchedID = ['v','i','n'];
+  console.log("likelist"+likeList);
+  //var matchIDs=[{}];
+  var matchedID = [];
+  var query = firebase.database().ref("Profiles/0/").orderByKey();
+query.once("value")
+  .then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      // key will be "ada" the first time and "alan" the second time
+      var key = childSnapshot.key;
+	   var childref = database.ref('Profiles/0/').child(key);
+		childref.on("value", function(snapshot) {
+        var likes = snapshot.child('likes').val();
+        var dislikes = snapshot.child('dislikes').val();
+		var vig=0;
+		if(checkEquals(likes,likeList)!=-1 && key!=profId){
+			console.log("matched"+ likes)
+			console.log(key);
+			matchedID.push(key);
+			vig=1;
+		}
+		if(checkEquals(dislikes,dislikeList)!=-1 && key!=profId && vig==0){
+			console.log("matched"+ likes)
+			console.log(key);
+			matchedID.push(key);
+		}
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+     
+      var childData = childSnapshot.val();
+	 // console.log(key+" "+childData);
+  });
+});
+
     return matchedID;
 }
 
@@ -53,4 +82,16 @@ function updatedMatchesRDB(profId,matchestoUpdate){
     console.log(matchesUpdated);
     var matchesRef = database.ref('matches/0/');
     matchesRef.child(profId).set(matchesUpdated);
+}
+
+function checkEquals(src,dest){
+	var check = -1;
+	for( var i=0; i<src.length;i++){
+		for(var j=0; j<src.length;j++){
+			if(src[i]==dest[j]){
+				check++;
+			}
+		}
+	}
+	return check;
 }
