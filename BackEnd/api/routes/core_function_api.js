3c4a2623775d8e2695afe68f940e8a7504d38b6b
Vinay <vinay.calastry@gmail.com>
2017-11-28 23:05:41 -0800

    Added root path

--- BackEnd/api/routes/core_function_api.js
+++ BackEnd/api/routes/core_function_api.js
@@ -3,4 +3,5 @@ const findMatches = require('../controller/find_matches.js');
 
 module.exports = function(app){
     app.get('/findmatch/:profId', findMatches.getMatches);
+	app.get('/', function(req, res){res.send("Amante GetMatches Service")});
 }
